306d57c0c6dd79ebe58a647435bbd3d5bbfe50e0
nadine.sonderegger <nadine.sonderegger@student.unisg.ch>
2020-04-16 13:14:49 +0200

    starting new sentiment analysis

---
+++ vader_sentiment.py
@@ -0,0 +1,55 @@
+# import SentimentIntensityAnalyzer class from vaderSentiment.vaderSentiment module.
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
+
+# Import dataset
+tweet_list = pd.read_csv('trumptweets.csv')
+
+# function to print sentiments of the sentence.
+
+
+def sentiment_scores(tweet):
+
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # oject gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+
+    print("Overall sentiment dictionary is : ", sentiment_dict)
+    print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+    print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+    print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+
+    print("Sentence Overall Rated As", end=" ")
+
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05:
+        print("Positive")
+
+    elif sentiment_dict['compound'] <= - 0.05:
+        print("Negative")
+
+    else:
+        print("Neutral")
+
+
+# Driver code
+if __name__ == "__main__":
+
+    print("\n1st statement :")
+    sentence = "Geeks For Geeks is the best portal for \
+                the computer science engineering students."
+
+    # function calling
+    sentiment_scores(sentence)
+
+    print("\n2nd Statement :")
+    sentence = "study is going on as usual"
+    sentiment_scores(sentence)
+
+    print("\n3rd Statement :")
+    sentence = "I am vey sad today."
+    sentiment_scores(sentence)
